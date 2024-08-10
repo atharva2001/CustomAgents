ca7a331c13a6346e0543ddaabdac80d66d15aece
Atharva Shirke <shirkeatharva.study@gmail.com>
2024-08-10 13:36:37 +0530

    updated tools

--- main.py
+++ main.py
@@ -3,18 +3,25 @@ from langchain.agents import create_json_agent, initialize_agent, AgentType
 from langchain_google_genai import ChatGoogleGenerativeAI 
 from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder, PromptTemplate
 from langchain.tools import tool
+import os 
 
-llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro-latest", google_api_key="You key here") 
+llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro-latest", google_api_key=os.getenv("GOOGLE_API_KEY")) 
 
 @tool 
 def getSum(n1: int, n2: int) -> int:
     """Return the sum of two numbers"""
     return n1 + n2 
 
-toolkit = [getSum]
+@tool 
+def getDiv(n1: int, n2: int) -> int:
+    """Return the divison of two numbers"""
+    return n1 // n2 
+
+toolkit = [getSum, getDiv]
 
 template = """
-        You are an mathematical Bot. Your task is to reutrn the sum of two numbers given to you.
+        You are an mathematical Bot. Your task is to reutrn the divison of two numbers given to you.
+        If you feel it gives an error perform the "getSum" operation.
 
         Begin!
 
